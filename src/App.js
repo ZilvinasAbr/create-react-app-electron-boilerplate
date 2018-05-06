5ca1722e1a16d2ce06b3fd411ec716441cabb365
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2018-05-06 20:30:50 +0300

    Init commit.

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  handleClick = () => {
+    let myNotification = new Notification('Title', {
+      body: 'Lorem Ipsum Dolor Sit Amet'
+    })
+    
+    myNotification.onclick = () => {
+      console.log('Notification clicked')
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={this.handleClick}>Notificate</button>
+      </div>
+    );
+  }
+}
+
+export default App;
